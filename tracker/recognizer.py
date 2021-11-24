5690ed7874902a217173dae63014167c9bbfdb01
Serge Grigorenko <serge@evercloud.io>
2021-11-24 22:20:35 +0200

    Update recognizer with canny detection

--- tracker/recognizer.py
+++ tracker/recognizer.py
@@ -1,14 +1,35 @@
 import logging
 
 import cv2
+import numpy as np
 
 
 class Recognizer:
-    """Provides API to recognize objects in a given image"""
+    """Provides API to recognize objects in a grayscale image"""
 
     def run(self, image, img_seq_num):
         prefix = "recognizer:"
-        output_path = "images/{}.png"
 
-        cv2.imwrite(output_path.format(img_seq_num), image)
-        logging.info(f"{prefix} image #{img_seq_num} saved")
+        blurred = cv2.GaussianBlur(image, (5, 5), 0)
+        edged = self.apply_canny(blurred, 0.33)
+
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+        largest_cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+        for c in largest_cnts:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+
+            if len(approx) == 4:
+                cv2.drawContours(image, [approx], -1, (255, 255, 255), 5)
+                cv2.imwrite(f"images/{img_seq_num}_edged.png", edged)
+                cv2.imwrite(f"images/{img_seq_num}.png", image)
+                logging.info(f"{prefix} image #{img_seq_num} recognized")
+                break
+
+    @staticmethod
+    def apply_canny(image, sigma):
+        m = np.median(image)
+        lower = max(0, (1.0 - sigma) * m)
+        upper = min(255, (1.0 + sigma) * m)
+        return cv2.Canny(image, int(lower), int(upper))
